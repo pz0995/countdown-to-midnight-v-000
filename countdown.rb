ff6dba6694fa003610509073919764cedb578cdf
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 23:21:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,12 +2,12 @@
 
 def countdown(count)
   count = x
-  count = 7
+  x = 7
   while count < 18
     puts "#{count} SECOND(S)"
     count -= 1
   end
-  if x==0
+  if count==0
     puts "HAPPY MEW YEAR!"
   end
 end
