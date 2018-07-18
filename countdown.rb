0aa4628e82a4ee4f3fa85c0d541a4d0871f1aebf
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 00:03:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  count = x
+  x = 15
+  while count < 10
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  if count==0
+    puts "HAPPY MEW YEAR!"
+  end
 end
+
+def countdown_with_sleep(count)
+  sleep 5
+  count = x
+  x = 15
+  while count < 10
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  if count==0
+    puts "HAPPY MEW YEAR!"
